424b2b413f17010af193979f19e7e5575542fe75
Jordi Garreta <jordigarreta11@gmail.com>
2025-04-07 11:16:17 +0200

    animation INFO page

--- app/info/info.js
+++ app/info/info.js
@@ -2,19 +2,63 @@
 import { useEffect, useRef, useState } from 'react';
 import styles from './page.module.scss';
 import { PortableText } from '@portabletext/react';
-import ContactForm from '@/components/ContactForm/ContactForm';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import Gradient from '@/components/Gradient/Gradient';
+import SplitType from 'split-type';
 
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.registerEffect({
+  name: 'counter',
+  extendTimeline: true,
+  defaults: {
+    end: 0,
+    duration: 0.5,
+    ease: 'power1',
+    increment: 1,
+    delay: 0,
+  },
+  effect: (targets, config) => {
+    let tl = gsap.timeline();
+    let num = targets[0].innerText.replace(/\,/g, '');
+    targets[0].innerText = num;
+
+    tl.to(
+      targets,
+      {
+        duration: config.duration,
+        innerText: config.end,
+        delay: config.delay,
+        //snap:{innerText:config.increment},
+        modifiers: {
+          innerText: function (innerText) {
+            return gsap.utils
+              .snap(config.increment, innerText)
+              .toString()
+              .replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+          },
+        },
+        ease: config.ease,
+      },
+      0
+    );
+
+    return tl;
+  },
+});
 
 const InfoWrapper = ({ data }) => {
   const imageRef = useRef(null);
   const wrapperRef = useRef(null);
+  const descriptionRef = useRef(null);
+  const itemsRef = useRef([]);
+  const numberRefs = useRef([]);
 
   /* Check if user uses GMAIL */
   const [emailHref, setEmailHref] = useState('');
+
+  // Check Mail
   useEffect(() => {
     const gmailHref =
       'https://mail.google.com/mail/u/0/?fs=1&to=jim@wilberg.studio&tf=cm';
@@ -26,10 +70,65 @@ const InfoWrapper = ({ data }) => {
     setEmailHref(isGmailUser ? gmailHref : mailtoHref);
   }, []);
 
+  // Animations
   useEffect(() => {
     setTimeout(() => {
       window.scrollTo(0, 0);
     }, 0);
+
+    // Description
+    const text = new SplitType(descriptionRef.current, {
+      types: 'chars,words',
+    });
+    const CHARS = text.chars;
+    gsap.fromTo(
+      CHARS,
+      {
+        autoAlpha: 0,
+        y: 0,
+      },
+      {
+        autoAlpha: 1,
+        y: 0,
+        stagger: 0.03,
+        duration: 0.1,
+        onComplete: () => {
+          if (imageRef.current) {
+            gsap.to(imageRef.current, {
+              opacity: 1,
+              duration: 1,
+              onComplete: () => {
+                gsap.fromTo(
+                  itemsRef.current,
+                  {
+                    opacity: 0,
+                    y: 10,
+                  },
+                  {
+                    opacity: 1,
+                    y: 0,
+                    stagger: 0.2,
+                  }
+                );
+                // Numbers
+                numberRefs.current.forEach((ref, index) => {
+                  const endValue = parseInt(data.items[index].number, 10);
+
+                  gsap.effects.counter(ref, {
+                    end: endValue,
+                    duration: 2,
+                    ease: 'power2.out',
+                    increment: 1,
+                    delay: index,
+                  });
+                });
+              },
+            });
+          }
+        },
+      }
+    );
+    // Image
     if (imageRef.current) {
       gsap.fromTo(
         imageRef.current,
@@ -49,7 +148,13 @@ const InfoWrapper = ({ data }) => {
         }
       );
     }
-  }, []);
+  }, [data.items]);
+
+  const addToRefs = (el) => {
+    if (el && !numberRefs.current.includes(el)) {
+      numberRefs.current.push(el);
+    }
+  };
 
   return (
     <div className={styles.page}>
@@ -58,26 +163,31 @@ const InfoWrapper = ({ data }) => {
         <img src={data.profile} alt='Page builder image' />
       </div>
       <div ref={wrapperRef} className={styles.page__wrapper}>
-        <div className={styles.page__wrapper__description}>
+        <div ref={descriptionRef} className={styles.page__wrapper__description}>
           <PortableText value={data.description} />
         </div>
 
         <div className={styles.page__wrapper__items}>
           {data.items.map((item, index) => (
-            <div className={styles.page__wrapper__items__data} key={index}>
-              <p className={styles.page__wrapper__items__data__number}>
-                {item.title}
-              </p>
+            <div
+              key={index}
+              className={styles.page__wrapper__items__data}
+              ref={(ref) => {
+                itemsRef.current[index] = ref;
+              }}
+            >
+              <div className={styles.page__wrapper__items__data__number}>
+                <p ref={addToRefs}>0</p>
+                <span>{item.title === 'VIEWS' ? 'm+' : '+'}</span>
+              </div>
               <p className={styles.page__wrapper__items__data__text}>
-                {item.number}
+                {item.title}
               </p>
             </div>
           ))}
         </div>
       </div>
       <div className={styles.page__contactform}>
-        {/* <p className={styles.page__contactform__title}>[ Contact Form ]</p>
-        <ContactForm /> */}
         <a
           className={styles.page__contactform__link}
           href={emailHref}
