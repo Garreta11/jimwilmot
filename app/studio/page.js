098106862643410262c0ac81f1bc79e63e259a8e
Jordi Garreta <jordigarreta11@gmail.com>
2025-07-16 10:58:36 +0200

    dynamic = 'force-dynamic';

--- app/studio/page.js
+++ app/studio/page.js
@@ -1,6 +1,8 @@
 import StudioWrapper from './StudioWrapper';
 import { getStudioProjects } from '../utils/sanity-utils';
 
+export const dynamic = 'force-dynamic';
+
 const Studios = async () => {
   const projects = await getStudioProjects();
   return <StudioWrapper projects={projects} />;
--- app/work/page.js
+++ app/work/page.js
@@ -1,6 +1,8 @@
 import { getAllCategories, getProjectsList } from '../utils/sanity-utils';
 import WorkPage from './work';
 
+export const dynamic = 'force-dynamic';
+
 const Work = async () => {
   const projects = await getProjectsList();
   const categories = await getAllCategories();
